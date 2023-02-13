0aeaa22e5c1a4550e694640510b1b40dba02c971
sudarshankapali <sudarshankapalisk@gmail.com>
2023-02-13 18:47:06 +0545

    Assignment_1

--- Sudarshan.java
+++ Sudarshan.java
@@ -1,6 +1,5 @@
 public class Sudarshan {
     public static void main(String[] args){
-        System.out.flush();
         System.out.println("Name of the organization: Kamalpokhari Resturant & Bar\n\nAddress:\n----State: KM\n----Zip Code: 44600\n----Country: Nepal\n\nContact Information:\n----Mobile Number: 9865382409\n----Email: kamalpokahri.rnb@gmail.com");
     }
 }
