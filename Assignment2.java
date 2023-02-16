8f0494486744a4a449e5e4c75622edd43c3b74ec
sudarshankapali <sudarshankapalisk@gmail.com>
2023-02-16 07:56:53 +0545

    Updated assignment 2

--- Assignment2.java
+++ Assignment2.java
@@ -5,19 +5,33 @@ public class Assignment2{
         System.out.println("What is the total number of employees?: \n");
         Scanner employeesNum = new Scanner(System.in);
         int employeesNum1 = employeesNum.nextInt();
-        System.out.println("What is the name of the supervisor?: \n");
+        System.out.println("\nWhat is the name of the supervisor?: \n");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         
-        System.out.println("how many times nuclear waste is reomved from the core: \n");
+        System.out.println("\nHow many times nuclear waste is reomved from the core: \n");
         Scanner waste = new Scanner(System.in);
         int newclearWaste = waste.nextInt();
-        System.out.println("What is the total weight of the waste?: \n");
+        System.out.println("\nWhat is the total weight of the waste?: \n");
         Scanner supervisorName = new Scanner(System.in);
         float supervisor = supervisorName.nextFloat();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Is the electric motor beign replaced every 18 days?: \n");
+        System.out.println("\nIs the electric motor beign replaced every 18 days? select 'y' for yes and 'n' for no: \n");
         char c = scanner.next().charAt(0);
-        System.out.println("You have entered: "+c);
+        System.out.println("\n\n\n\tUpdate on Nuclear Reactor name: JX00-XC-HB1\n\t Location: East of Japan\n\n");
+        System.out.println("The total number of employees are: "+employeesNum1);
+        System.out.println("The name of the supervisor is : "+name);
+        System.out.println("The total number of tiems neculear waste removed is  "+newclearWaste +" times");
+        System.out.println("The weight of the waste is : "+supervisor);
+        if(c == 'y' || c == 'Y'){
+            System.out.println("You have entered: "+c);
+        }
+        else if (c == 'n' || c == 'N'){
+            System.out.println("You have entered: "+c);
+        }
+        else{
+            System.out.println("Invalid input:");
+        }
+        
     }
 }
