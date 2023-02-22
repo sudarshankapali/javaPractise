0f25c29722acde9ea7aed9f226f0a5abed101a37
sudarshankapali <sudarshankapalisk@gmail.com>
2023-02-22 07:40:49 +0545

    ForeachLoop

---
+++ Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach{
+    public static void main(String[] arg){
+        int[] number = {1,2,3,4,5};
+        for(int num: number){
+            System.out.println(num);
+        }
+    }
+}
