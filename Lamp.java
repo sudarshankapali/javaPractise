375046a8401637b8a1f91a14e3ff4deb9c1d099e
sudarshankapali <sudarshankapalisk@gmail.com>
2023-02-24 11:11:49 +0545

    Practising_Class_And_Object

---
+++ Lamp.java
@@ -0,0 +1,11 @@
+class Lamp{
+    boolean mode;
+     void turnOn(){
+        mode = true;
+        System.out.println("Light is ON? "+mode);
+     }
+     void turnOff(){
+        mode = false;
+        System.out.println("Lighr is ON?"+mode);
+     }
+}
---
+++ classObject.java
@@ -0,0 +1,9 @@
+
+
+public class classObject{
+    public static void main(String[] arg){
+        Lamp bulb = new Lamp();
+        bulb.turnOn();
+        bulb.turnOff();
+    }
+}
