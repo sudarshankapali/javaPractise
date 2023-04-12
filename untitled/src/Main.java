dabe68612cf01c298c925d4637dbd07267d19de7
sudarshankapali <sudarshankapalisk@gmail.com>
2023-04-12 11:39:04 +0545

    understanding java connector

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import com.mysql.cj.protocol.Resultset;
 
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class .forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost/sudarshan";
+        Connection conn = DriverManager.getConnection(url,"root","root");
+        System.out.println("connection success");
+
+        //lets create the query
+        Statement stm = conn.createStatement();
+        ResultSet rs =  stm.executeQuery("select * from new_table");
+//        System.out.println(rs.getNextResultset());
+        while (rs.next()){
+            String id = rs.getString("ID");
+            String name = rs.getString("Name");
+            String address = rs.getString("Address");
+            System.out.println(id);
+            System.out.println(name);
+            System.out.println(address);
+        }
     }
 }
