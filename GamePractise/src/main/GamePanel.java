28dde8592fa045aefaf83ae78e7f86d5680d1b6c
sudarshankapali <sudarshankapalisk@gmail.com>
2023-06-15 09:31:18 +0545

    images

--- GamePractise/src/main/GamePanel.java
+++ GamePractise/src/main/GamePanel.java
@@ -3,26 +3,48 @@ package main;
 import inputs.MouseInputs;
 import inputs.keyboardInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class GamePanel extends JPanel {
     private float xDelta=100,yDelta=100;
     private MouseInputs mouseInputs;
     private int frames = 0;
     private long lastCheck = 0;
-    private float xDir = 0.2f,yDir = 0.2f;
-    private Random random;
-    private Color color = new Color(150,20,90);
+    private float xDir = 0.9f,yDir = 0.9f;
+    private BufferedImage img,subImg;
+
     public GamePanel(){
-        random = new Random();
+        importImg();
         mouseInputs = new MouseInputs(this);
+        setPanelSize();
         addKeyListener(new keyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
-public void changeXdelta(int value){
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280,800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
+    public void changeXdelta(int value){
 this.xDelta+=value;
 }
 public void changeYdelta(int value){
@@ -35,9 +57,9 @@ this.yDelta = y;
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int)xDelta,(int)yDelta,200,50);
+//        g.drawImage(img.getSubimage(0,0,64,40),0,0,128,80,null);
+        subImg = img.getSubimage(1*64,8*40,64,40);
+        g.drawImage(subImg,(int)xDelta,(int)yDelta,128,80,null);
         frames++;
                 if(System.currentTimeMillis() - lastCheck >= 1000){
                     lastCheck = System.currentTimeMillis();
@@ -46,22 +68,5 @@ this.yDelta = y;
                 }
 //        repaint();
     }
-    private void updateRectangle(){
-        xDelta+=xDir;
-        if(xDelta>400 || xDelta<0){
-            xDir*= -1;
-            color=getRndColor();
-        }
-        yDelta+=yDir;
-        if(yDelta>400 || yDelta<0){
-            yDir*= -1;
-            color=getRndColor();
-        }
-    }
-    private Color getRndColor(){
-        int r= random.nextInt(255);
-        int g= random.nextInt(255);
-        int b= random.nextInt(255);
-        return new Color(r,g,b);
-    }
+
 }
--- GamePractise/src/main/GameWindow.java
+++ GamePractise/src/main/GameWindow.java
@@ -6,10 +6,12 @@ public class GameWindow {
     private JFrame jFrame;
     public GameWindow(GamePanel gamePanel){
         jFrame = new JFrame();
-        jFrame.setSize(400,400);
+//        jFrame.setSize(400,400);
         jFrame.setLocationRelativeTo(null);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
+        jFrame.setResizable(false);
+        jFrame.pack();
         jFrame.setVisible(true);
 
     }
